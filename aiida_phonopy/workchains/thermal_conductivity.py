201e1754231ced633d3585690567aedaf218bddd
abel <abelcarreras83@gmail.com>
2018-01-16 13:02:11 +0900

    Added a WorkChain for automatic calculation of thermal conductivity

--- aiida_phonopy/workchains/thermal_conductivity.py
+++ aiida_phonopy/workchains/thermal_conductivity.py
@@ -6,7 +6,7 @@ from aiida.work.workchain import WorkChain, ToContext
 from aiida.work.workfunction import workfunction
 from aiida.work.run import run, submit, async
 
-from aiida.orm import load_node, DataFactory, WorkflowFactory
+from aiida.orm import load_node, DataFactory, WorkflowFactory, CalculationFactory, Code
 from aiida.orm.data.base import Str, Float, Bool, Int
 from aiida.work.workchain import _If, _While
 
@@ -19,18 +19,56 @@ ParameterData = DataFactory('parameter')
 ArrayData = DataFactory('array')
 StructureData = DataFactory('structure')
 
-BandStructureData = DataFactory('phonopy.band_structure')
-GruneisenPhonopy = WorkflowFactory('phonopy.gruneisen')
 PhononPhonopy = WorkflowFactory('phonopy.phonon')
+PhononPhono3py = WorkflowFactory('phonopy.phonon3')
 
-OptimizeStructure = WorkflowFactory('phonopy.optimize')
 
-class PhononPhono3py(WorkChain):
+def generate_phono3py_params(code, structure, ph_settings, force_sets, nac_data=None):
+    """
+    Generate inputs parameters needed to do a remote phonopy calculation
 
+    :param code: Code object of phono3py
+    :param structure: StructureData Object that constains the crystal structure unit cell
+    :param ph_settings: ParametersData object containing a dictionary with the phonopy input data
+    :param force_sets: ForceSetsData object containing the atomic forces and displacement information
+    :param force_sets: NacData object containing the dielectric tensor and Born effective charges info
+    :return: Calculation process object, input dictionary
+    """
+
+    plugin = code.get_attr('input_plugin')
+    PhonopyCalculation = CalculationFactory(plugin)
+
+    # The inputs
+    inputs = PhonopyCalculation.process().get_inputs_template()
+
+    # code
+    inputs.code = code
+
+    # structure
+    inputs.structure = structure
+
+    # parameters
+    inputs.parameters = ph_settings
+
+    # resources
+    inputs._options.resources = ph_settings.dict.machine['resources']
+    inputs._options.max_wallclock_seconds = ph_settings.dict.machine['max_wallclock_seconds']
+
+    # data_sets
+    inputs.data_sets = force_sets
+
+    # non-analytical corrections
+    if nac_data is not None:
+        inputs.nac_data = nac_data
+
+    return PhonopyCalculation.process(), inputs
+
+
+class ThermalPhono3py(WorkChain):
 
     @classmethod
     def define(cls, spec):
-        super(PhononPhono3py, cls).define(spec)
+        super(ThermalPhono3py, cls).define(spec)
         spec.input("structure", valid_type=StructureData)
         spec.input("ph_settings", valid_type=ParameterData)
         spec.input("es_settings", valid_type=ParameterData)
@@ -39,6 +77,7 @@ class PhononPhono3py(WorkChain):
         spec.input("pressure", valid_type=Float, required=False, default=Float(0.0))
         spec.input("use_nac", valid_type=Bool, required=False, default=Bool(False))  # false by default
         spec.input("chunks", valid_type=Int, required=False, default=Int(100))
+        spec.input("step", valid_type=Float, required=False, default=Int(2.0))
 
         spec.outline(cls.harmonic_calculation,
                      _While(cls.not_converged)(cls.get_data_sets,
@@ -47,18 +86,75 @@ class PhononPhono3py(WorkChain):
 
     def not_converged(self):
 
-        pass
+        is_converged = False
+
+        if 'old_conductivity' in self.ctx:
+            self.ctx.conductivity = self.ctx.anharmonic.out.kappa.get_array('kappa')
+            is_converged = np.allclose(self.ctx.conductivity, self.ctx.old_conductivity, rtol=0.3, atol=0.1)
+
+        if 'anharmonic' in self.ctx:
+            self.ctx.iteration += 1
+            self.ctx.cutoff += float(self.inputs.step) * self.ctx.iteration
+            self.ctx.old_conductivity = self.ctx.conductivity
+            self.ctx.input_data_sets = self.ctx.anharmonic.out.data_sets
+
+        else:
+            self.ctx.cutoff = self.inputs.step
+            self.ctx.iteration = 1
+            self.ctx.input_data_sets = self.ctx.harmonic.out.data_sets
+            self.ctx.final_structure = self.ctx.harmonic.out.final_structure
+            self.out('final_structure', self.ctx.final_structure)
+
+            if not self.ctx.harmonic.out.dos.is_stable(tol=1e-3):
+                self.report('This structure is not dynamically stable. Workchain will stop')
+                exit()
+
+
+        return not is_converged
 
     def harmonic_calculation(self):
-        pass
+
+        print('start thermal conductivity (pk={})'.format(self.pid))
+
+        self.report('Harmonic calculation')
+
+        future = submit(PhononPhonopy,
+                        structure=self.inputs.structure,
+                        ph_settings=self.inputs.ph_settings,
+                        es_settings=self.inputs.es_settings,
+                        pressure=self.inputs.pressure,
+                        optimize=self.inputs.optimize,
+                        use_nac=self.inputs.use_nac
+                        )
+        return ToContext(harmonic=future)
 
     def get_data_sets(self):
-        pass
+
+        future = submit(PhononPhono3py,
+                        structure=self.ctx.final_structure,
+                        ph_settings=self.inputs.ph_settings,
+                        es_settings=self.inputs.es_settings,
+                        optimize=Bool(False),
+                        use_nac=Bool(False),
+                        cutoff=Float(self.ctx.cutoff),
+                        chunks=self.inputs.chunks,
+                        data_sets=self.ctx.input_data_sets,
+                        )
+
+        return ToContext(anharmonic=future)
 
     def get_thermal_conductivity(self):
 
-        pass
+        code_label = self.inputs.ph_settings.get_dict()['code']
+
+        JobCalculation, calculation_input = generate_phono3py_params(code=Code.get_from_string(code_label),
+                                                                     structure=self.ctx.final_structure,
+                                                                     ph_settings=self.inputs.ph_settings,
+                                                                     force_sets=self.ctx.input_data_sets)
+        future = submit(JobCalculation, **calculation_input)
+        return ToContext(thermal_conductivity=future)
 
     def collect_data(self):
 
-        pass
+        self.out('kappa', self.ctx.thermal_conductivity.out.kappa)
+        self.report('finish thermal conductivity')
