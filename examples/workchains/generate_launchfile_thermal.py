da1acce6b13de65ad85bb9d2de11eb33d490674d
yw-fang <fyuewen@gmail.com>
2018-02-09 00:05:04 +0900

    give a alternative choice to vasp.5.x

--- examples/workchains/generate_launchfile_thermal.py
+++ examples/workchains/generate_launchfile_thermal.py
@@ -7,6 +7,8 @@
 import os
 from os.path import exists
 import numpy as np
+import ase.units
+from ase.utils import basestring
 
 #define spaces: four spaces == indent_N (N=1,2,3..)
 indent_1, indent_2, indent_3, indent_4 = (' '*4, ' '*8, ' '*12, ' '*16)
@@ -29,6 +31,99 @@ if  exists(title):
     else:
         pass
 
+
+
+#Read scale, lattice vectors, atomic coordinates from POSCAR
+def read_vasp(filename='POSCAR'):
+    from ase import Atoms
+    from ase.constraints import FixAtoms, FixScaled
+    from ase.data import chemical_symbols
+    #    import numpy as np
+    if isinstance(filename, basestring):
+        f = open(filename)
+    else:  # Assume it's a file-like object
+        f = filename
+    """
+    By default I think you are using the POSCAR with the format
+    that comes with vasp.5.x, in that case, the headline in POSCAR
+    is useless, and the 6th line lists elements. 
+    
+    In case you are using POSCAR in vasp.4.x that does not include
+    the element in the sixth line, we also provide the solution in this code.
+    In that case, the headline usually shows the elements information
+    """
+    #read the first two lines of POSCAR
+    poscar_headline = f.readline()
+    print(poscar_headline)
+    scale = float(f.readline().split()[0])
+    """
+    I put .split() here in case there could be typo errors in the
+    second line of POSCAR
+    """
+    print(scale)
+    # Read lattice vectors
+    scaled_lattice_vector = []
+    for ii in range(3):
+        string_vect = f.readline().split()
+        float_vect=list(map(float, string_vect))
+        """
+        an alternative way of converting string_vect to float_vect is
+        float_vect=[(float(string_vect(0)), float(string_vect(1)),
+        float(string_vect(2))]
+        """
+        scaled_lattice_vector.append(float_vect)
+    print(scaled_lattice_vector)
+    lattice_vector = scale * np.array(scaled_lattice_vector)
+    """
+    it should be mentioned that, in some python library like ase, the
+    lattice_vector in my code is defined as 'basis vector', but it's wrong.
+    They are two different concepts, in POSCAR, lattice vectors are used.
+    """
+    print(lattice_vector)
+
+    """
+    In future, I may remove the feature supporting vasp.4.x
+    At that time, I will pass the elements to atom_symbols directly
+    """
+    #This part is four future use if only vasp.5.x is supported
+    #read elements and the numbers of atoms.
+    """
+    atom_symbols = f.readline().split()
+    print(atom_symbols)
+    str_num_of_atom_list = f.readline().split()
+    int_num_of_atom_list = list(map(int, str_num_of_atom_list))
+    total_num_of_atoms=sum(int_num_of_atom_list)
+    print(total_num_of_atoms)
+    
+    """
+
+    """
+    Currently I still support the feature of vasp.4.x.
+    Here, the code will check which format we have.
+    If the format is vasp.5.x, the 6th line will be atomic symbols
+    It it is vasp.4.x format, the 6th line will be number of atoms
+    """
+    atom_symbols = []
+    str_num_of_atom_list = f.readline().split()
+
+    vasp5 = False
+    try:
+        int(str_num_of_atom_list[0])
+        #if it's vasp.5, str_num_of_atom_list[0] must be a str, can
+        #cannot be converted into integer
+    except ValueError:
+        vasp5 = True
+        atom_symbols = str_num_of_atom_list
+        print(atom_symbols)
+        str_num_of_atom_list = f.readline().split()
+        int_num_of_atom_list = list(map(int, str_num_of_atom_list))
+        total_num_of_atoms = sum(int_num_of_atom_list)
+        print(total_num_of_atoms)
+#        numofatoms = f.readline().split()
+
+
+
+
 with open(title,'wt') as f:
     f.write('#############################################################\n')
     f.write('# This script is for generating the launch scirpt used for  #\n')
@@ -47,17 +142,18 @@ with open(title,'wt') as f:
     f.write("ParameterData = DataFactory('parameter')\n")
     f.write("import numpy as np\n")
     f.write("# Define structure\n")
+#    POSCAR = open('POSCAR', 'rt', encoding='utf-8')
+    read_vasp()
 
-    with open('POSCAR', 'rt', encoding='utf-8') as f1:
-        line_number=0
-        for line in f1:
-            line_number=line_number+1
-            if line_number==2:
-                a = float(line)
-                print(a,type(a))
-            if line_number==3:
-                lattice_vector_1=line.split()
-                lattice_vector_1=list(map(float, lattice_vector_1))
-                lattice_vector_1=np.array(lattice_vector_1)
-                print(lattice_vector_1[:],type(lattice_vector_1))
-
+#    with open('POSCAR', 'rt', encoding='utf-8') as f1:
+#        line_number=0
+#        for line in f1:
+#            line_number=line_number+1
+#            if line_number==2:
+#                scale = float(line)
+#                print(scale,type(scale))
+#            if line_number==3:
+#                lattice_vector_1=line.split()
+#                lattice_vector_1=list(map(float, lattice_vector_1))
+#                lattice_vector_1=np.array(lattice_vector_1)
+#                print(lattice_vector_1[:],type(lattice_vector_1))
