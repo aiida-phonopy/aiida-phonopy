5581bb97e049ce3c2bbd43d85ae8c8744b100cc5
bastonero <bastonero.lorenzo@gmail.com>
2023-05-09 17:08:13 +0000

    Fix 5

--- docs/source/local_module/__init__.py
+++ docs/source/local_module/__init__.py
@@ -8,8 +8,8 @@ os.environ['AIIDA_PATH'] = str(pathlib.Path(__file__).parent / '_aiida_path')
 # load the configuration without emitting a warning
 with warnings.catch_warnings():
     warnings.simplefilter('ignore', UserWarning)
-    from aiida import manage
+    from aiida.manage.configuration import settings
 
-    manage.configuration.settings
+    settings
 
 from .temp_profile import load_temp_profile  # noqa: F401
